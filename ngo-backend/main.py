a940ae0fc22d917eed08c81808c03d18efef0797
apurva-ak47 <apurvakohad12@gmail.com>
2026-04-25 15:53:56 +0530

    changed the const API

--- ngo-backend/main.py
+++ ngo-backend/main.py
@@ -22,8 +22,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
+        "http://ngo-frontend.s3-website-us-east-1.amazonaws.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
--- ngo-frontend/src/services/api.js
+++ ngo-frontend/src/services/api.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "http://18.234.55.208:8000",
   timeout: 10000,
 });
-
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) req.headers.Authorization = `Bearer ${token}`;
